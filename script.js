5f0ca8ad123e058bb9e90e31232aed47d79934c1
rennan costa <rennan_s.costa@hotmail.com>
2021-04-11 19:30:18 -0300

    html done css done problems with js

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+var principal = document.getElementById("principal").value;
+var rate = document.getElementById("rate").value;
+var years = document.getElementById("years").value;
+
+output.innerHTML=rate.value
+
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+ 
+
+    var interest = principal * years * rate / 100;
+    var today = new Date();
+    var beginning = today.getFullYear();
+    var end = beginning + years;
+
+    alert("bla", interest,end);
+
+}
+
+rate.oninput=function()
+{
+   output.innerHTML=this.value
 }
         
